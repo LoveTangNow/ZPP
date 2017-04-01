7d5e5d9a4b0bc622fe3c6ca40128113e64e032bb
zhaoguocheng <15021863473@163.com>
2017-04-02 00:10:31 +0800

    Button

--- app/src/main/java/com/jano/zpp/StartActivity.java
+++ app/src/main/java/com/jano/zpp/StartActivity.java
@@ -2,8 +2,11 @@ package com.jano.zpp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
